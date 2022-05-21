12dad534682f2d4d163e379fe4e804054d43ac22
Charles Delachapelle <91090132+Charlless22@users.noreply.github.com>
2022-05-21 14:36:55 +0200

    update

--- src/text.c
+++ src/text.c
@@ -21,3 +21,153 @@ void text(game_t *game)
     sfText_setPosition(game->text, game->position);
     sfRenderWindow_drawText(game->window, game->text, NULL);
 }
+
+void first_question(game_t *game)
+{
+    game->str = "Comment se prénomme la fille intello de la famille Simpson ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void second_question(game_t *game)
+{
+    game->str = "Comment se prénomme le farceur de la famille Simpson ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void third_question(game_t *game)
+{
+    game->str = "Comment se prénomme le père de la famille Simpson ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void fourth_question(game_t *game)
+{
+    game->str = "Comment se prénomme la mère de la famille Simpson ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void fifth_question(game_t *game)
+{
+    game->str = "Comment se prénomme le bébé de la famille Simpson ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void sixth_question(game_t *game)
+{
+    game->str = "Quel est le prénom de Bart ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void seventh_question(game_t *game)
+{
+    game->str = "Quel est le prénom complet de Maggie ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void eighth_question(game_t *game)
+{
+    game->str = "Qui est le meilleur ami de Bart ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void ninth_question(game_t *game)
+{
+    game->str = "Où travaille Homer ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
+
+void tenth_question(game_t *game)
+{
+    game->str = " Marge était elle flic ?";
+    game->font = sfFont_createFromFile("./assets/homer.ttf");
+    game->text = sfText_create();
+    sfText_setCharacterSize(game->text, 75);
+    sfText_setString(game->text, game->str);
+    sfText_setFont(game->text, game->font);
+    sfText_setColor(game->text, sfYellow);
+    game->position.x = 720;
+    game->position.y = 0;
+    sfText_setPosition(game->text, game->position);
+    sfRenderWindow_drawText(game->window, game->text, NULL);
+}
