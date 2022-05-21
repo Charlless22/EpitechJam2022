a06948f94a03ef926d80f1ffc7fb76e711bd5cee
corentin coullet <corentin.coullet@epitech.eu>
2022-05-21 22:05:01 +0200

    update question

--- src/questions2.c
+++ src/questions2.c
@@ -43,13 +43,28 @@ void is_button_7(game_t *game)
     int x = sfMouse_getPositionRenderWindow(game->window).x;
     int y = sfMouse_getPositionRenderWindow(game->window).y;
 
-    if (sfMouse_isButtonPressed) {
-        if (x >= game->obj[1]->pose.x && x <= game->obj[1]->pose.x + 256) {
-            if (y >= game->obj[1]->pose.y && y <= game->obj[1]->pose.y + 65) {
+    if (x >= game->obj[13]->pose.x && x <= game->obj[13]->pose.x + 256) {
+        if (y >= game->obj[13]->pose.y && y <= game->obj[13]->pose.y + 65) {
                 game->note += 1;
                 game->question -= 1;
                 printf("note = %d\n", game->note);
-            }
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[11]->pose.x && x <= game->obj[11]->pose.x + 256) {
+        if (y >= game->obj[11]->pose.y && y <= game->obj[11]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[12]->pose.x && x <= game->obj[12]->pose.x + 256) {
+        if (y >= game->obj[12]->pose.y && y <= game->obj[12]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
         }
     }
 }
@@ -59,13 +74,28 @@ void is_button_8(game_t *game)
     int x = sfMouse_getPositionRenderWindow(game->window).x;
     int y = sfMouse_getPositionRenderWindow(game->window).y;
 
-    if (sfMouse_isButtonPressed) {
-        if (x >= game->obj[1]->pose.x && x <= game->obj[1]->pose.x + 256) {
-            if (y >= game->obj[1]->pose.y && y <= game->obj[1]->pose.y + 65) {
+    if (x >= game->obj[13]->pose.x && x <= game->obj[13]->pose.x + 256) {
+        if (y >= game->obj[13]->pose.y && y <= game->obj[13]->pose.y + 65) {
                 game->note += 1;
                 game->question -= 1;
                 printf("note = %d\n", game->note);
-            }
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[11]->pose.x && x <= game->obj[11]->pose.x + 256) {
+        if (y >= game->obj[11]->pose.y && y <= game->obj[11]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[12]->pose.x && x <= game->obj[12]->pose.x + 256) {
+        if (y >= game->obj[12]->pose.y && y <= game->obj[12]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
         }
     }
 }
@@ -75,13 +105,28 @@ void is_button_9(game_t *game)
     int x = sfMouse_getPositionRenderWindow(game->window).x;
     int y = sfMouse_getPositionRenderWindow(game->window).y;
 
-    if (sfMouse_isButtonPressed) {
-        if (x >= game->obj[1]->pose.x && x <= game->obj[1]->pose.x + 256) {
-            if (y >= game->obj[1]->pose.y && y <= game->obj[1]->pose.y + 65) {
+    if (x >= game->obj[13]->pose.x && x <= game->obj[13]->pose.x + 256) {
+        if (y >= game->obj[13]->pose.y && y <= game->obj[13]->pose.y + 65) {
                 game->note += 1;
                 game->question -= 1;
                 printf("note = %d\n", game->note);
-            }
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[11]->pose.x && x <= game->obj[11]->pose.x + 256) {
+        if (y >= game->obj[11]->pose.y && y <= game->obj[11]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[12]->pose.x && x <= game->obj[12]->pose.x + 256) {
+        if (y >= game->obj[12]->pose.y && y <= game->obj[12]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
         }
     }
 }
@@ -91,13 +136,28 @@ void is_button_10(game_t *game)
     int x = sfMouse_getPositionRenderWindow(game->window).x;
     int y = sfMouse_getPositionRenderWindow(game->window).y;
 
-    if (sfMouse_isButtonPressed) {
-        if (x >= game->obj[1]->pose.x && x <= game->obj[1]->pose.x + 256) {
-            if (y >= game->obj[1]->pose.y && y <= game->obj[1]->pose.y + 65) {
+    if (x >= game->obj[13]->pose.x && x <= game->obj[13]->pose.x + 256) {
+        if (y >= game->obj[13]->pose.y && y <= game->obj[13]->pose.y + 65) {
                 game->note += 1;
                 game->question -= 1;
                 printf("note = %d\n", game->note);
-            }
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[11]->pose.x && x <= game->obj[11]->pose.x + 256) {
+        if (y >= game->obj[11]->pose.y && y <= game->obj[11]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
+        }
+    }
+    if (x >= game->obj[12]->pose.x && x <= game->obj[12]->pose.x + 256) {
+        if (y >= game->obj[12]->pose.y && y <= game->obj[12]->pose.y + 65) {
+                game->note += 0;
+                game->question -= 1;
+                printf("note = %d\n", game->note);
+                sleep(2);
         }
     }
 }
