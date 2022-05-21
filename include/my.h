6e278e49c42c6e41d4f092a0051030a6f82f241f
Charles Delachapelle <91090132+Charlless22@users.noreply.github.com>
2022-05-21 11:11:08 +0200

    update

--- include/my.h
+++ include/my.h
@@ -18,6 +18,9 @@
     #include <stdio.h>
     #include <unistd.h>
     #include <string.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
 
 typedef struct object_s {
     char *name;
